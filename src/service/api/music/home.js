d4b5c4f979d0b459100bd60a02edc9e636e7c1ed
zzkuai <zzkuai@gmail.com>
2021-03-13 19:45:51 +0800

    fix: 修复接口请求地址

--- src/service/api/music/home.js
+++ src/service/api/music/home.js
@@ -5,15 +5,15 @@ import { httpGet } from '~src/service/fetch'
 
 // 获取banner
 export function getBanner(type = 1) {
-  return httpGet('music-api/banner', { type })
+  return httpGet('/music-api/banner', { type })
 }
 
 // 获取首页信息
 export function getHomeInfo() {
-  return httpGet('music-api/homepage/block/page')
+  return httpGet('/music-api/homepage/block/page')
 }
 
 // 获取推荐入口
 export function getEntry() {
-  return httpGet('music-api/homepage/dragon/ball')
+  return httpGet('/music-api/homepage/dragon/ball')
 }
